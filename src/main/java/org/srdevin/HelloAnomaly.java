c2e5de75f8bd5a749607d98d9e5b00bb0b457759
ravink <nkravikumar90@gmail.com>
2017-04-18 12:32:51 +0530

    Adding test classes

---
+++ src/main/java/org/srdevin/HelloAnomaly.java
@@ -0,0 +1,11 @@
+package org.srdevin;
+
+/**
+ * Created by rnk on 4/18/2017.
+ */
+public class HelloAnomaly {
+    public static void main(String ... args){
+
+        System.out.println("Hello !! This solution detects the anomalies and there root cause for the anomalies");
+    }
+}
---
+++ src/main/scala/org/srdevin/HelloAnoDetection.scala
@@ -0,0 +1,10 @@
+package org.srdevin
+
+/**
+  * Created by rnk on 4/18/2017.
+  */
+object HelloAnoDetection {
+  def main(args: Array[String]): Unit = {
+    println("Hello !! This solution detects the anomalies and there root cause for the anomalies")
+  }
+}
